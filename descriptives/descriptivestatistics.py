2c36a346368bd492b4dff58a5e62402602bc0cb2
matthewdippel <mdippel@ccs.neu.edu>
2017-09-22 17:26:23 -0400

    distribution plots cut on categorical data

--- descriptives/descriptivestatistics.py
+++ descriptives/descriptivestatistics.py
@@ -9,23 +9,65 @@ def print_descriptive_statistics(df):
         print('-----')
         print("type: %s" % str(df[col].dtype))
         if df[col].dtype == np.float64 or df[col].dtype == np.int64:
-            print(df[col].describe())
+            print(df[col].describe(percentiles=[.1, .2, .3, .4, .5, .6, .7, .8, .9]))
+            print("Skewness: %f" % df[col].skew())
+            print("Kurtosis: %f" % df[col].kurt())
         else:
-            print("unique count: %s" % str(df[col].nunique()))
             print(df[col].unique())
+            print(df[col].describe())
         print("")
 
 
-def create_descriptive_histograms(df):
+def create_descriptive_histograms(df, save_dir):
     for col in df:
-        print(df[col])
+        #print(df[col])
         if df[col].dtype == np.float64 or df[col].dtype == np.int64:
-            plotutils.pretty_histogram(df, col)
+            ax = plotutils.pretty_histogram(df, col)
+            fname = None
+            if save_dir is not None:
+                fname = save_dir + "histogram-%s.pdf" % col
+            plotutils.save_ax_as_file(ax, fname)
         else:
             seq = df[col].value_counts()
-            plotutils.pretty_bar(seq)
+            ax = plotutils.pretty_bar(seq)
+            fname = None
+            if save_dir is not None:
+                fname = save_dir + "histogram-%s.pdf" % col
+            plotutils.save_ax_as_file(ax, fname)
     return
 
+def create_all_numeric_scatterplots(df, save_dir):
+    numeric_columns = []
+    for col in df:
+        if df[col].dtype == np.float64 or df[col].dtype == np.int64:
+            numeric_columns.append(col)
+    for i in range(len(numeric_columns)):
+        for j in range(i):
+            c1 = numeric_columns[i]
+            c2 = numeric_columns[j]
+            ax = plotutils.pretty_scatter(df, c1, c2)
+            fname = None
+            if save_dir is not None:
+                fname = save_dir + "scatter-%s-vs-%s.pdf" % (c1, c2)
+            plotutils.save_ax_as_file(ax, fname)
+
+def create_all_categorical_split_distributions(df, save_dir):
+    numeric_columns = []
+    object_columns = []
+    for col in df:
+        if df[col].dtype == np.float64 or df[col].dtype == np.int64:
+            numeric_columns.append(col)
+        elif df[col].dtype == np.object or df[col].dtype == np.str:
+            object_columns.append(col)
+    for cat_col in object_columns:
+        for data_col in numeric_columns:
+            ax = plotutils.categorical_distribution(df, data_col=data_col, cat_col=cat_col)
+            fname = None
+            if save_dir is not None:
+                fname = save_dir + "categorical-%s-cut_on-%s.pdf" % (data_col, cat_col)
+            plotutils.save_ax_as_file(ax, fname)
+            
+        
 
 if __name__ == "__main__":
     import pandas as pd
@@ -34,4 +76,6 @@ if __name__ == "__main__":
     iris_df = pd.read_csv(dir + "/../sampledata/iris.csv")
     
     print_descriptive_statistics(iris_df)
-    create_descriptive_histograms(iris_df)
+    create_descriptive_histograms(iris_df, '/Users/mdippel/test_plots/')
+    create_all_numeric_scatterplots(iris_df, '/Users/mdippel/test_plots/')
+    create_all_categorical_split_distributions(iris_df, '/Users/mdippel/test_plots/')
--- plots/plotutils.py
+++ plots/plotutils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 __author__ = 'mdippel'
 
 
@@ -8,24 +9,42 @@ def prettify_axis(ax):
     ax.grid(linestyle='-', linewidth='0.5', color='white')
     
 def pretty_scatter(df, x, y):
+    plt.clf()
     ax = df.plot(kind='scatter', x=x, y=y)
     prettify_axis(ax)
-    plt.show()
+    return ax
     
 def pretty_bar(seq):
+    plt.clf()
     ax = seq.plot(kind='bar')
     prettify_axis(ax)
-    plt.show()
+    return ax
 
 def pretty_histogram(df, col):
-    ax = df[col].plot(kind='hist', bins=100)
+    plt.clf()
+    ax = sns.distplot(df[col], bins=100)
     ax.set_xlabel(col)
     prettify_axis(ax)
-    plt.show()
+    return ax
+
+def categorical_distribution(df, data_col, cat_col):
+    plt.clf()
+    import pandas as pd
+    data = pd.concat([df[data_col], df[cat_col]])
+    ax = sns.boxplot(x=cat_col, y=data_col, data=df)
+    prettify_axis(ax)
+    return ax
+    
+def save_ax_as_file(ax, fname):
+    fig = ax.get_figure()
+    if fname is not None:
+        fig.savefig(fname)
+
     
 if __name__ == "__main__":
     import pandas as pd
     import os
+    import numpy as np
     dir = os.path.dirname(os.path.abspath(__file__))
     iris_df = pd.read_csv(dir + "/../sampledata/iris.csv")
     #ax = iris_df.plot()
@@ -36,4 +55,10 @@ if __name__ == "__main__":
     #        pretty_scatter(iris_df, c1, c2)
     for c in numeric_columns:
         pretty_histogram(iris_df, c)
+    
+    for col in iris_df:
+        if not (iris_df[col].dtype == np.float64 or iris_df[col].dtype == np.int64):
+            for data_col in numeric_columns:
+                ax = categorical_distribution(iris_df, data_col, col)
+                plt.show()
     
